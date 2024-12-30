71a4361e180e58613b4729bd888f716edd41e9dd
Susanta Biswas <17782600+susantabiswas@users.noreply.github.com>
2024-12-30 18:46:19 +0530

    lc3 binary machine code reader and trap codes

--- VirtualMachine/lc3_vm.cpp
+++ VirtualMachine/lc3_vm.cpp
@@ -68,7 +68,83 @@ enum COND {
 
 #pragma endregion Condition Flag
 
-int main() {
+#pragma region Trap code
+
+// Trap codes are associated with piece of code called Trap routines. These
+// are OS routines which the user level programs can use to achieve some functionality.
+// Usually because these operations are high-priviledge ops, the cpu mode is changed to
+// kernel mode and then the os runs the trap routine.
+// In the context of this VM, these trap codes are mapped to the trap routines written for
+// this VM alone, internally those functions might make system calls which will then invoke
+// the OS Trap routines.
+enum TrapCode {
+    TRAP_GETC = 0x20, // Get a char from keyboard, no stdout echo
+    TRAP_OUT = 0x21, // print a char
+    TRAP_PUTS = 0x22, // print a word string
+    TRAP_IN = 0x23, // get a char from keyboard, stdout echo
+    TRAP_PUTSP = 0x24, //print a byte string
+    TRAP_HALT = 0x25 // halt program execution
+};
+
+#pragma endregion Trap code
+
+#pragma region VM utils
+
+uint16_t swap_byte_layout16(uint16_t val) {
+    return (val << 8) | (val >> 8);
+}
+
+void read_image_file(FILE* file) {
+    // the LC3 machine code file starts with a 16-bit value that represents the starting address of the program
+    // we will load the contents of the file into the memory starting from this address.
+    uint16_t origin = 0;
+    // read only the 1st line, this will give us the starting address of the program
+    fread(&origin, sizeof(uint16_t), 1, file);
+
+    // max no. of memory words that can be placed if we start from origin
+    int max_lines = MEMORY_MAX - origin;
+
+    // pos in memory where the file will be loaded
+    uint16_t* file_ptr = memory + origin;
+
+    // read the remaining data
+    uint16_t lines_read = fread(file_ptr, sizeof(uint16_t), max_lines, file);
+
+    // the lc3 machine code uses big-endian, so we will convert the data to little-endian
+    // as our machine is little-endian
+    for(int i = 0; i < lines_read; i++) {
+        *file_ptr = swap_byte_layout16(*file_ptr);
+        ++file_ptr;
+    }
+
+    cout << "Loaded image file into memory, size: " << lines_read * 2 << " Bytes" << endl;
+}
+
+bool load_image(const char* path) {
+    cout << "Image path: " << path << endl;
+
+    FILE* img_file = fopen(path, "rb");
+    if (!img_file)
+        return false;
+    
+    read_image_file(img_file);
+    fclose(img_file);
+    return true;
+}
+
+#pragma endregion VM utils
+
+int main(int argc, const char* argv[]) {
+    if (argc < 2) {
+        cout << "Usage: lc3 <image-file>\n";
+        exit(2); 
+    }
+
+    if (!load_image(argv[1])) {
+        cout << "LC3 image load failed\n";
+        exit(1);
+    }
+
     cout << "LC-3 VM running..." << endl;
     return 0;
 }
